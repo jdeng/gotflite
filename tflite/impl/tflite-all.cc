f2cae50fcac4b0d1c454848d8e4ebc932e4a07a5
Jack <jackdeng@gmail.com>
2019-02-22 12:40:14 -0800

    android support

--- tflite/impl/tflite-all.cc
+++ tflite/impl/tflite-all.cc
@@ -120,7 +120,6 @@ extern "C" {
 #include "fft4g.c"
 }
 
-
 #include "tensorflow/lite/kernels/eigen_support.cc"
 
 #include "farmhash.cc"
---
+++ tflite/tflite_android.go
@@ -0,0 +1,9 @@
+// +build android
+package tflite
+
+//#cgo LDFLAGS: -llog 
+//#cgo CFLAGS: -I.
+// #include "ndk/cpu-features.c"
+import "C"
+
+
