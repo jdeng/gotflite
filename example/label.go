c9851babc4dcc1344b4b828c586bdb73fa547f66
Jack <jackdeng@gmail.com>
2019-02-23 11:27:17 -0800

    t

--- example/label.go
+++ example/label.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"github.com/jdeng/gotflite"
-	"image"
-	"os"
+	"github.com/jdeng/gotflite/recognizer"
+	"io/ioutil"
 )
 
 var (
@@ -18,47 +16,21 @@ var (
 func main() {
 	flag.Parse()
 
-	// load labels
-	synset, err := os.Open(*labelsFile)
-	if err != nil {
-		panic(err)
-	}
-	dict := []string{}
-	scanner := bufio.NewScanner(synset)
-	for scanner.Scan() {
-		dict = append(dict, scanner.Text())
-	}
-
-	// load image
-	f, err := os.Open(*imageFile)
-	if err != nil {
-		panic(err)
-	}
-	img, _, err := image.Decode(f)
+	model, err := recognizer.New(*modelFile, *labelsFile)
 	if err != nil {
 		panic(err)
 	}
 
-	// load model
-	pred, err := gotflite.NewPredictor(*modelFile, 224, 224, 0)
+	img, err := ioutil.ReadFile(*imageFile)
 	if err != nil {
 		panic(err)
 	}
-	defer pred.Release()
 
 	// run model
-	out, err := pred.Run(img)
+	n := 10
+	results, err := recognizer.Run(model, img, n)
 	if err != nil {
-		fmt.Printf("%v\n", err)
 		panic(err)
 	}
-
-	index := make([]int, len(out))
-	gotflite.Argsort(out, index)
-
-	fmt.Println("Top results:")
-	for i := 0; i < 10; i++ {
-		fmt.Printf(" %d, %f, %s\n", index[i], out[i], dict[index[i]])
-	}
-	fmt.Println("")
+	fmt.Printf("Top %d results: %v\n", n, results)
 }
